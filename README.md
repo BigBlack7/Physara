8c8d16d5dd36e9c9a9cf61cbbb4a253833c2cf41
bigblack <bigblack@qq.com>
2026-04-18 16:38:30 +0800

    初始化Physara仓库

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Physara
+
+- Build it by MSVC with C++20.
+- A Physical-based Rasteraztion Renderer.
+- Refer to Filament and so on.
---
+++ Runtime/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <filesystem>
+
+int main()
+{
+    std::cout << std::filesystem::current_path() << std::endl;
+
+    return 0;
+}
