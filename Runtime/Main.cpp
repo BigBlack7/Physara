4c99bc35f9853e3cb0bef030de33ba984ff6157e
bigblack <bigblack@qq.com>
2026-04-19 16:21:34 +0800

    部分第三方库引入并简单测试

--- Runtime/Main.cpp
+++ Runtime/Main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 #include <filesystem>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <entt/entt.hpp>
+#include <imgui/imgui.h>
+#include <imgui/ImGuizmo.h>
+#include <nlohmann/json.hpp>
+#include <tinygltf/tiny_gltf_v3.h>
 
 int main()
 {
-    std::cout << std::filesystem::current_path() << std::endl;
-
+    glm::mat4 l(1.f);
+    entt::registry registry;
+    ImGui::CreateContext();
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Hello World", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    nlohmann::json json;
+    std::cout << l[0][0] << std::endl;
     return 0;
 }
